b64539d5ea17faf0929cbf1648b5eaf141e272ee
virusbear <fabianfahrenholz718@gmail.com>
2023-07-04 20:11:29 +0200

    Add vector operations to IVec4 class
    Removed the unused import "kotlin.math.pow". Added pre-defined vectors: ONE, UNIT_X, UNIT_Y, UNIT_Z, UNIT_W, and some vector operations to IVec4 class including addition, subtraction, unary minus, multiplication, division and dot product methods. Also, implemented ways to get the length, squared length, and distance (or squared distance) to another vector. The changes were made to facilitate vector math operations.

--- tinn-math/src/main/kotlin/com/virusbear/tinn/math/IVec4.kt
+++ tinn-math/src/main/kotlin/com/virusbear/tinn/math/IVec4.kt
@@ -1,6 +1,5 @@
 package com.virusbear.tinn.math
 
-import kotlin.math.pow
 import kotlin.math.sqrt
 
 data class IVec4(
@@ -11,9 +10,45 @@ data class IVec4(
 ) {
     companion object {
         val ZERO = IVec4(0, 0, 0, 0)
+        val ONE = IVec4(1, 1, 1, 1)
+        val UNIT_X = IVec4(1, 0, 0, 0)
+        val UNIT_Y = IVec4(0, 1, 0, 0)
+        val UNIT_Z = IVec4(0, 0, 1, 0)
+        val UNIT_W = IVec4(0, 0, 0, 1)
     }
 
+    val squaredLength: Double =
+        x.toDouble() * x.toDouble() + y.toDouble() * y.toDouble() + z.toDouble() * z.toDouble() + w.toDouble() * w
+
     val length by lazy {
-        sqrt(x.toDouble().pow(2) + y.toDouble().pow(2) + z.toDouble().pow(2) + w.toDouble().pow(2))
+        sqrt(squaredLength)
     }
+
+    operator fun plus(other: IVec4): IVec4 =
+        IVec4(x + other.x, y + other.y, z + other.z, w + other.w)
+    operator fun plus(scalar: Number): IVec4 =
+        IVec4(x + scalar.toInt(), y + scalar.toInt(), z + scalar.toInt(), w + scalar.toInt())
+
+    operator fun minus(other: IVec4): IVec4 =
+        IVec4(x - other.x, y - other.y, z - other.z, w - other.w)
+    operator fun minus(scalar: Number): IVec4 =
+        IVec4(x - scalar.toInt(), y - scalar.toInt(), z - scalar.toInt(), w - scalar.toInt())
+
+    operator fun unaryMinus(): IVec4 =
+        IVec4(-x, -y, -z, -w)
+
+    operator fun times(scalar: Number): IVec4 =
+        IVec4(x * scalar.toInt(), y * scalar.toInt(), z * scalar.toInt(), w * scalar.toInt())
+
+    operator fun div(scalar: Number): IVec4 =
+        IVec4(x / scalar.toInt(), y / scalar.toInt(), z / scalar.toInt(), w / scalar.toInt())
+
+    infix fun dot(v: IVec4): Double =
+        x.toDouble() * v.x.toDouble() + y.toDouble() * v.y.toDouble() + z.toDouble() * v.z.toDouble() + w.toDouble() * v.w.toDouble()
+
+    fun squaredDistanceTo(other: IVec4): Double =
+        (other - this).squaredLength
+
+    fun distanceTo(other: IVec4): Double =
+        sqrt(squaredDistanceTo(other))
 }
