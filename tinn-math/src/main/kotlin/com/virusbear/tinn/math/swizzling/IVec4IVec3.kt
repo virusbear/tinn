843f6e2bf94adc0d73d99397810bf011278ca512
virusbear <fabianfahrenholz718@gmail.com>
2023-07-06 18:18:08 +0200

    Add swizzling properties to Vec4 to Vec3 conversion

---
+++ tinn-math/src/main/kotlin/com/virusbear/tinn/math/swizzling/IVec4IVec3.kt
@@ -0,0 +1,133 @@
+package com.virusbear.tinn.math.swizzling
+
+import com.virusbear.tinn.math.IVec3
+import com.virusbear.tinn.math.IVec4
+
+val IVec4.xxx: IVec3
+    get() = IVec3(x, x, x)
+val IVec4.xxy: IVec3
+    get() = IVec3(x, x, y)
+val IVec4.xxz: IVec3
+    get() = IVec3(x, x, z)
+val IVec4.xxw: IVec3
+    get() = IVec3(x, x, w)
+val IVec4.xyx: IVec3
+    get() = IVec3(x, y, x)
+val IVec4.xyy: IVec3
+    get() = IVec3(x, y, y)
+val IVec4.xyz: IVec3
+    get() = IVec3(x, y, z)
+val IVec4.xyw: IVec3
+    get() = IVec3(x, y, w)
+val IVec4.xzx: IVec3
+    get() = IVec3(x, z, x)
+val IVec4.xzy: IVec3
+    get() = IVec3(x, z, y)
+val IVec4.xzz: IVec3
+    get() = IVec3(x, z, z)
+val IVec4.xzw: IVec3
+    get() = IVec3(x, z, w)
+val IVec4.xwx: IVec3
+    get() = IVec3(x, w, x)
+val IVec4.xwy: IVec3
+    get() = IVec3(x, w, y)
+val IVec4.xwz: IVec3
+    get() = IVec3(x, w, z)
+val IVec4.xww: IVec3
+    get() = IVec3(x, w, w)
+val IVec4.yxx: IVec3
+    get() = IVec3(y, x, x)
+val IVec4.yxy: IVec3
+    get() = IVec3(y, x, y)
+val IVec4.yxz: IVec3
+    get() = IVec3(y, x, z)
+val IVec4.yxw: IVec3
+    get() = IVec3(y, x, w)
+val IVec4.yyx: IVec3
+    get() = IVec3(y, y, x)
+val IVec4.yyy: IVec3
+    get() = IVec3(y, y, y)
+val IVec4.yyz: IVec3
+    get() = IVec3(y, y, z)
+val IVec4.yyw: IVec3
+    get() = IVec3(y, y, w)
+val IVec4.yzx: IVec3
+    get() = IVec3(y, z, x)
+val IVec4.yzy: IVec3
+    get() = IVec3(y, z, y)
+val IVec4.yzz: IVec3
+    get() = IVec3(y, z, z)
+val IVec4.yzw: IVec3
+    get() = IVec3(y, z, w)
+val IVec4.ywx: IVec3
+    get() = IVec3(y, w, x)
+val IVec4.ywy: IVec3
+    get() = IVec3(y, w, y)
+val IVec4.ywz: IVec3
+    get() = IVec3(y, w, z)
+val IVec4.yww: IVec3
+    get() = IVec3(y, w, w)
+val IVec4.zxx: IVec3
+    get() = IVec3(z, x, x)
+val IVec4.zxy: IVec3
+    get() = IVec3(z, x, y)
+val IVec4.zxz: IVec3
+    get() = IVec3(z, x, z)
+val IVec4.zxw: IVec3
+    get() = IVec3(z, x, w)
+val IVec4.zyx: IVec3
+    get() = IVec3(z, y, x)
+val IVec4.zyy: IVec3
+    get() = IVec3(z, y, y)
+val IVec4.zyz: IVec3
+    get() = IVec3(z, y, z)
+val IVec4.zyw: IVec3
+    get() = IVec3(z, y, w)
+val IVec4.zzx: IVec3
+    get() = IVec3(z, z, x)
+val IVec4.zzy: IVec3
+    get() = IVec3(z, z, y)
+val IVec4.zzz: IVec3
+    get() = IVec3(z, z, z)
+val IVec4.zzw: IVec3
+    get() = IVec3(z, z, w)
+val IVec4.zwx: IVec3
+    get() = IVec3(z, w, x)
+val IVec4.zwy: IVec3
+    get() = IVec3(z, w, y)
+val IVec4.zwz: IVec3
+    get() = IVec3(z, w, z)
+val IVec4.zww: IVec3
+    get() = IVec3(z, w, w)
+val IVec4.wxx: IVec3
+    get() = IVec3(w, x, x)
+val IVec4.wxy: IVec3
+    get() = IVec3(w, x, y)
+val IVec4.wxz: IVec3
+    get() = IVec3(w, x, z)
+val IVec4.wxw: IVec3
+    get() = IVec3(w, x, w)
+val IVec4.wyx: IVec3
+    get() = IVec3(w, y, x)
+val IVec4.wyy: IVec3
+    get() = IVec3(w, y, y)
+val IVec4.wyz: IVec3
+    get() = IVec3(w, y, z)
+val IVec4.wyw: IVec3
+    get() = IVec3(w, y, w)
+val IVec4.wzx: IVec3
+    get() = IVec3(w, z, x)
+val IVec4.wzy: IVec3
+    get() = IVec3(w, z, y)
+val IVec4.wzz: IVec3
+    get() = IVec3(w, z, z)
+val IVec4.wzw: IVec3
+    get() = IVec3(w, z, w)
+val IVec4.wwx: IVec3
+    get() = IVec3(w, w, x)
+val IVec4.wwy: IVec3
+    get() = IVec3(w, w, y)
+val IVec4.wwz: IVec3
+    get() = IVec3(w, w, z)
+val IVec4.www: IVec3
+    get() = IVec3(w, w, w)
--- tinn-math/src/main/kotlin/com/virusbear/tinn/math/swizzling/Vec4Vec3.kt
+++ tinn-math/src/main/kotlin/com/virusbear/tinn/math/swizzling/Vec4Vec3.kt
@@ -1,2 +1,133 @@
 package com.virusbear.tinn.math.swizzling
 
+import com.virusbear.tinn.math.Vec3
+import com.virusbear.tinn.math.Vec4
+
+val Vec4.xxx: Vec3
+    get() = Vec3(x, x, x)
+val Vec4.xxy: Vec3
+    get() = Vec3(x, x, y)
+val Vec4.xxz: Vec3
+    get() = Vec3(x, x, z)
+val Vec4.xxw: Vec3
+    get() = Vec3(x, x, w)
+val Vec4.xyx: Vec3
+    get() = Vec3(x, y, x)
+val Vec4.xyy: Vec3
+    get() = Vec3(x, y, y)
+val Vec4.xyz: Vec3
+    get() = Vec3(x, y, z)
+val Vec4.xyw: Vec3
+    get() = Vec3(x, y, w)
+val Vec4.xzx: Vec3
+    get() = Vec3(x, z, x)
+val Vec4.xzy: Vec3
+    get() = Vec3(x, z, y)
+val Vec4.xzz: Vec3
+    get() = Vec3(x, z, z)
+val Vec4.xzw: Vec3
+    get() = Vec3(x, z, w)
+val Vec4.xwx: Vec3
+    get() = Vec3(x, w, x)
+val Vec4.xwy: Vec3
+    get() = Vec3(x, w, y)
+val Vec4.xwz: Vec3
+    get() = Vec3(x, w, z)
+val Vec4.xww: Vec3
+    get() = Vec3(x, w, w)
+val Vec4.yxx: Vec3
+    get() = Vec3(y, x, x)
+val Vec4.yxy: Vec3
+    get() = Vec3(y, x, y)
+val Vec4.yxz: Vec3
+    get() = Vec3(y, x, z)
+val Vec4.yxw: Vec3
+    get() = Vec3(y, x, w)
+val Vec4.yyx: Vec3
+    get() = Vec3(y, y, x)
+val Vec4.yyy: Vec3
+    get() = Vec3(y, y, y)
+val Vec4.yyz: Vec3
+    get() = Vec3(y, y, z)
+val Vec4.yyw: Vec3
+    get() = Vec3(y, y, w)
+val Vec4.yzx: Vec3
+    get() = Vec3(y, z, x)
+val Vec4.yzy: Vec3
+    get() = Vec3(y, z, y)
+val Vec4.yzz: Vec3
+    get() = Vec3(y, z, z)
+val Vec4.yzw: Vec3
+    get() = Vec3(y, z, w)
+val Vec4.ywx: Vec3
+    get() = Vec3(y, w, x)
+val Vec4.ywy: Vec3
+    get() = Vec3(y, w, y)
+val Vec4.ywz: Vec3
+    get() = Vec3(y, w, z)
+val Vec4.yww: Vec3
+    get() = Vec3(y, w, w)
+val Vec4.zxx: Vec3
+    get() = Vec3(z, x, x)
+val Vec4.zxy: Vec3
+    get() = Vec3(z, x, y)
+val Vec4.zxz: Vec3
+    get() = Vec3(z, x, z)
+val Vec4.zxw: Vec3
+    get() = Vec3(z, x, w)
+val Vec4.zyx: Vec3
+    get() = Vec3(z, y, x)
+val Vec4.zyy: Vec3
+    get() = Vec3(z, y, y)
+val Vec4.zyz: Vec3
+    get() = Vec3(z, y, z)
+val Vec4.zyw: Vec3
+    get() = Vec3(z, y, w)
+val Vec4.zzx: Vec3
+    get() = Vec3(z, z, x)
+val Vec4.zzy: Vec3
+    get() = Vec3(z, z, y)
+val Vec4.zzz: Vec3
+    get() = Vec3(z, z, z)
+val Vec4.zzw: Vec3
+    get() = Vec3(z, z, w)
+val Vec4.zwx: Vec3
+    get() = Vec3(z, w, x)
+val Vec4.zwy: Vec3
+    get() = Vec3(z, w, y)
+val Vec4.zwz: Vec3
+    get() = Vec3(z, w, z)
+val Vec4.zww: Vec3
+    get() = Vec3(z, w, w)
+val Vec4.wxx: Vec3
+    get() = Vec3(w, x, x)
+val Vec4.wxy: Vec3
+    get() = Vec3(w, x, y)
+val Vec4.wxz: Vec3
+    get() = Vec3(w, x, z)
+val Vec4.wxw: Vec3
+    get() = Vec3(w, x, w)
+val Vec4.wyx: Vec3
+    get() = Vec3(w, y, x)
+val Vec4.wyy: Vec3
+    get() = Vec3(w, y, y)
+val Vec4.wyz: Vec3
+    get() = Vec3(w, y, z)
+val Vec4.wyw: Vec3
+    get() = Vec3(w, y, w)
+val Vec4.wzx: Vec3
+    get() = Vec3(w, z, x)
+val Vec4.wzy: Vec3
+    get() = Vec3(w, z, y)
+val Vec4.wzz: Vec3
+    get() = Vec3(w, z, z)
+val Vec4.wzw: Vec3
+    get() = Vec3(w, z, w)
+val Vec4.wwx: Vec3
+    get() = Vec3(w, w, x)
+val Vec4.wwy: Vec3
+    get() = Vec3(w, w, y)
+val Vec4.wwz: Vec3
+    get() = Vec3(w, w, z)
+val Vec4.www: Vec3
+    get() = Vec3(w, w, w)
