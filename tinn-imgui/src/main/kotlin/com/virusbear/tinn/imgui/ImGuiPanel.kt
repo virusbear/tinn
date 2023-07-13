36f0aa163c90b5371d177a6e921d6ac4ffb150ef
virusbear <fabianfahrenholz718@gmail.com>
2023-07-13 22:03:47 +0200

    Removed imgui in favor of custom UI framework

--- tinn-imgui/src/main/kotlin/com/virusbear/tinn/imgui/ImGuiPanel.kt
+++
@@ -1,7 +0,0 @@
-package com.virusbear.tinn.imgui
-
-import com.virusbear.tinn.ui.Panel
-
-interface ImGuiPanel: Panel {
-    fun windowFlags(): Int
-}
--- tinn-studio/build.gradle.kts
+++ tinn-studio/build.gradle.kts
@@ -12,11 +12,6 @@ dependencies {
     implementation(project(":tinn-math"))
     implementation(project(":tinn-color"))
     implementation(project(":tinn-opengl"))
-    implementation(project(":tinn-imgui"))
     implementation(project(":tinn-nodes"))
     implementation(project(":tinn-core"))
-
-    implementation("io.github.spair:imgui-java-binding:$imguiVersion")
-    implementation("io.github.spair:imgui-java-lwjgl3:$imguiVersion")
-    implementation("io.github.spair:imgui-java-natives-windows:$imguiVersion")
 }
