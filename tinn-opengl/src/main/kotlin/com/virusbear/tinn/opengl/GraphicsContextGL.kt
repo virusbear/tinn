0e377f7e3f13a124931dd3f5cb7880c1b3a85608
virusbear <fabianfahrenholz718@gmail.com>
2023-11-18 11:37:57 +0000

    Refactor GraphicsContextGL for improved concurrency
    This commit simplifies the GraphicsContextGL class by removing the custom executor service and replacing it with a RenderThread. The changes consolidate the control logic for making OpenGL context, including error handling and clean up processes. This refactor improves efficiency by reducing the redundant code and facilitates easier management of codebase.
    
    
    Merge-request: TINN-MR-32
    Merged-by: Fabian Fahrenholz <fabianfahrenholz718@gmail.com>

--- tinn-opengl/src/main/kotlin/com/virusbear/tinn/opengl/GraphicsContextGL.kt
+++ tinn-opengl/src/main/kotlin/com/virusbear/tinn/opengl/GraphicsContextGL.kt
@@ -2,68 +2,28 @@ package com.virusbear.tinn.opengl
 
 import com.virusbear.tinn.GraphicsContext
 import com.virusbear.tinn.Driver
+import com.virusbear.tinn.RenderThread
 import com.virusbear.tinn.Trackable
 import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFW.glfwMakeContextCurrent
 import org.lwjgl.system.MemoryUtil.NULL
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-class GraphicsContextGL(driver: Driver): Trackable(driver), GraphicsContext {
+class GraphicsContextGL(
+    override val driver: Driver
+): GraphicsContext {
+    override val renderThread: RenderThread = RenderThreadGL()
+
     var native: Long = NULL
         private set
-    private val owner: ExecutorService = Executors.newSingleThreadExecutor()
 
-    fun makeCurrent(context: Long) {
-        ensureAlive()
-
-        clearCurrent()
+    fun makeContextCurrent(context: Long) {
         native = context
-        glfwMakeContextCurrent()
-    }
 
-    fun clearCurrent() {
-        ensureAlive()
-
-        native = NULL
-        glfwMakeContextCurrent()
-    }
-
-    fun <T> execute(block: () -> T): T {
-        ensureAlive()
-        return owner.submit(block).get()
-    }
-
-
-    fun <T> future(block: () -> T): CompletableFuture<T> {
-        ensureAlive()
-        val future = CompletableFuture<T>()
-
-        owner.submit {
-            future.complete(block())
+        renderThread.schedule {
+            glfwMakeContextCurrent(native)
         }
-
-        return future
-    }
-
-    fun schedule(block: () -> Unit) {
-        ensureAlive()
-        owner.submit(block)
-    }
-
-    override fun destroy() {
-        super.destroy()
-
-        owner.shutdown()
-    }
-
-    private fun glfwMakeContextCurrent() {
-        schedule {
-            GLFW.glfwMakeContextCurrent(native)
-        }
-    }
-
-    private fun ensureAlive() {
-        require(!destroyed) { "Trying to run in destroyed Context" }
     }
 }
